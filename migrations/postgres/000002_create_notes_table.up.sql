1672a6b7ddccb709ee44e5af75fb13a80f5da7cc
Viacheslav Davydov <dv0vd@dv0vd.xyz>
2025-10-12 18:58:50 +0300

    Migrations for notes table

---
+++ migrations/postgres/000002_create_notes_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE notes (
+    id            BIGSERIAL                  NOT NULL                   PRIMARY KEY,
+    title         VARCHAR(255)               NOT NULL,
+    description   TEXT,
+    user_id       BIGSERIAL                  NOT NULL                   REFERENCES users(id)   ON UPDATE RESTRICT   ON DELETE RESTRICT,
+    created_at    TIMESTAMP WITH TIME ZONE   NOT NULL   DEFAULT NOW(),
+    updated_at    TIMESTAMP WITH TIME ZONE   NOT NULL   DEFAULT NOW()
+);
